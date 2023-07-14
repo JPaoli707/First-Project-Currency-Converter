4d26589ae6b3bae15034134cce64c730aa2f0a9d
Electricman707 <139590741+Electricman707@users.noreply.github.com>
2023-07-14 17:23:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,19 @@ int main() {
   std::cout << "Input USD Amount: " << "\n";
   std::cin >> usd;
   
-  std::cout << "What form would you like to convert this too?" << "\n";
+  std::cout << "What form would you like to convert this too:" << "\n" 
+    << "Euro" << "\n"
+    << "Yuan" << "\n"
+    << "Peso";
   std::cin >> form;
+
+if form = "Euro" {
+
+  
+
+  
+}
+  
   
   
 
