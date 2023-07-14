f5a8a7fdfa703d546b911d2326002d986e2de3bf
Electricman707 <139590741+Electricman707@users.noreply.github.com>
2023-07-14 17:16:30 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main() {
 
   std::cout << "Input USD Amount: " << "\n";
   std::cin >> usd;
-
+  
+  std::cout << "What form would you like to convert this too?" << "\n";
+  std::cin >> form;
   
   
 
