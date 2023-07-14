a56da0fc0657f4297dfd3207f0bae17c6862be4b
Electricman707 <139590741+Electricman707@users.noreply.github.com>
2023-07-14 17:35:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,40 @@ int main() {
   std::cin >> usd;
   
   std::cout << "What form would you like to convert this too:" << "\n" 
-    << "Euro" << "\n"
-    << "Yuan" << "\n"
-    << "Peso";
+    << "- Euro" << "\n"
+    << "- Yuan" << "\n"
+    << "- Peso";
   std::cin >> form;
 
+
+//European Euro Conversion:
+  
 if form = "Euro" {
 
-  int amount_U2E = usd * 0.8894
+  int amount_U2E = usd * 0.89;
 
+  std::cout << "Your amount is " << amount_U2E << "Euros" << "\n";
+  
+}
+
+//Chinese Yuan Conversion:
+
+if form = "Yuan" {
+
+  int amount_U2Y = usd * 7.14;
+  
+  std::cout << "Your amount is " << amount_U2Y << "Yuan" << "\n";
+  
+}
+
+//Mexican Peso Conversion:
+
+if form = "Peso" {
+
+  int amount_U2P = usd * 16.75;
+  
+  std::cout << "Your amount is " << amount_U2P << "Pesos" << "\n";
   
 }
   
-  
-  
-
 }
